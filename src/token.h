fb92a47e1c596c31154762e8553798e7570db3d2
kolacik33 <kolacik33@pm.me>
2024-05-16 11:05:08 +0000

    converted Token into a template

--- src/token.h
+++ src/token.h
@@ -8,10 +8,18 @@ public:
   TokenType type;
   OperatorType optype;
   AtomBase *atom;
-  Token(TokenType t);
-  Token(TokenType t, OperatorType o);
-  Token(TokenType t, AtomBase *a);
-  void print();
+  Token(TokenType t): type(t), atom(NULL) {}
+  Token(TokenType t, OperatorType o): type(t), optype(o), atom(NULL) {};
+  Token(TokenType t, AtomBase *a): type(t), atom(a) {};
+  void print() {
+    if (type==EMPTY_TOKEN) {
+      std::cout << "Token(EMPTY)" << std::endl;
+    } else if (type==OPERATOR_TOKEN) {
+      std::cout << "Token(OPERATOR " << optype << ")" << std::endl;
+    } else if (type==ATOM_TOKEN) {
+      std::cout << "Token(ATOM " << atom << ")" << std::endl;
+    }
+  };
 };
 
 #endif //TOKEN
